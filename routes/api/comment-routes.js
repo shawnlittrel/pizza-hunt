c18271bedadf28a5e928a5e117038b0b005638b8
Shawn Littrel <shawnlittrel@protonmail.com>
2021-05-24 17:17:04 -0700

    comment model done

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { addComment, removeComment } = require('../../controllers/comment-controller');
 
-// /api/comments/pizzaID
+// /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
-// /api/comments/pizzaID/commentID
+// /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
 module.exports = router;
